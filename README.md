b7a647e6104a9e4d250fe3c3d5c0a97f27bd8b19
Beau Malroney <bamalroney@outlook.com>
2024-08-11 21:49:40 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # fictional-engine
 LLM to Read from Documents
+
+# My Python Script: [llama_sidecar.py]
+
+This is a Python script that interacts with an LLM and includes information from local documentation. It requires Python 3.11 and can be run on Windows/MAC/Linux.
+
+**Getting Started**
+
+1. **Install dependencies**: Run the following command in your terminal/command prompt to create a virtual
+environment (venv) and install the required packages:
+```
+python -m venv myenv
+myenv\Scripts\activate
+pip install -r requirements.txt
+```
+This will create a new directory called `myenv` containing the virtual environment, activate it, and then install
+all the dependencies listed in the `requirements.txt` file.
+
+2. **Run the script**: Once you've installed the dependencies, you can run the script using:
+```
+python llama_sidecar.py
+```
+
+**Script Requirements**
+
+* Python 3.11
+* The following packages are required:
+        + llama-index-llms-ollama
+        + llama-index
+        + llama-index-embeddings-huggingface
+        + docx2txt
+
+**License**
+
+This script is released under the unlicense license. See the LICENSE file for more information.
--- llama_sidecar.py
+++ llama_sidecar.py
@@ -1,40 +1,53 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core.llms import ChatMessage
 
-llm = Ollama(model="llama3", request_timeout=120.0)
+documents = SimpleDirectoryReader("documents").load_data()
 
-def chatMessageTest():
-    messages = [
-        ChatMessage(
-            role="system", content="You are a pirate with a colorful personality"
-        ),
-        ChatMessage(role="user", content="What is your name"),
-    ]
-    resp = llm.chat(messages)
-    print(resp)
-    return
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=120.0)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
+
+query_engine = index.as_query_engine()
+print(query_engine.query("What can you tell me about zenva?"))
+
+# def chatMessageTest():
+#     messages = [
+#         ChatMessage(
+#             role="system", content="You are a pirate with a colorful personality"
+#         ),
+#         ChatMessage(role="user", content="What is your name"),
+#     ]
+#     resp = llm.chat(messages)
+#     print(resp)
+#     return
 
 
-def test_chat():
-    resp = llm.complete("Who is Paul Graham?")
-    print(resp)
-    return
+# def test_chat():
+#     resp = llm.complete("Who is Paul Graham?")
+#     print(resp)
+#     return
 
-def testStream():
-    response = llm.stream_complete("Who is Paul Graham?")
-    for r in response:
-        print(r.delta, end="")
-    return
+# def testStream():
+#     response = llm.stream_complete("Who is Paul Graham?")
+#     for r in response:
+#         print(r.delta, end="")
+#     return
 
-def testStreamChatMessage():
-    messages = [
-        ChatMessage(
-            role="system", content="You are a pirate with a colorful personality"
-        ),
-        ChatMessage(role="user", content="What is your name"),
-    ]
-    resp = llm.stream_chat(messages)
-    for r in resp:
-        print(r.delta, end="")
-
-testStreamChatMessage()
+# def testStreamChatMessage():
+#     messages = [
+#         ChatMessage(
+#             role="system", content="You are a pirate with a colorful personality"
+#         ),
+#         ChatMessage(role="user", content="What is your name"),
+#     ]
+#     resp = llm.stream_chat(messages)
+#     for r in resp:
+#         print(r.delta, end="")
